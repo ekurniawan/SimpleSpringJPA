5265c74e892a230ecb15ca9faaee8a0c8e82f07c
ekurniawan <erick.kurniawan@gmail.com>
2022-10-13 16:05:22 +0700

    menambahkan class untuk security

--- src/main/java/com/rapidtech/springdatajpapgsql/repository/UserDaoRepository.java
+++ src/main/java/com/rapidtech/springdatajpapgsql/repository/UserDaoRepository.java
@@ -4,4 +4,5 @@ import com.rapidtech.springdatajpapgsql.model.DAOUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDaoRepository extends JpaRepository<DAOUser,Long> {
+    DAOUser findByUsername(String username);
 }
